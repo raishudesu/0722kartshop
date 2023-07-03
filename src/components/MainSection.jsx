842bf0e37f023daae59e5174d9969608311be3a0
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-07-03 20:39:26 +0800

    added merch.json

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { AiOutlineCheckCircle } from 'react-icons/ai'
+import Merch from "../assets/merch.json"
 
 const MainSection = () => {
   return (
@@ -10,230 +11,22 @@ const MainSection = () => {
       </div>
       <div className="flex justify-center">
         <ul className="grid grid-cols-2 gap-5 sm:grid-cols-4 md:grid-cols-5">
-          <li className="  max-w-[200px] flex flex-col gap-3">
-            <a href="">
-              <img
-                src="https://cdn-contents.weverseshop.io/public/shop/4d11e76df0007e851de705a70c936944.png?q=95&w=320"
-                alt="/"
-                className=""
-              />
-            </a>
-            <div className="flex flex-col justify-start gap-2 p-2">
-              <h6 className="uppercase text-xs font-bold">Pre-order</h6>
-              <h4 className="text-sm">
-              NewJeans 2nd EP 'Get Up' Bunny Beach Bag ver. Set
-              </h4>
-              <h6 className="font-bold">$111.48</h6>
-            </div>
-          </li>
-          <li className=" max-w-[200px] flex flex-col gap-3">
-            <a href="">
-              <img
-                src="https://cdn-contents.weverseshop.io/public/shop/82626250f824e4d047166786310a8393.png?q=95&w=320"
-                alt="/"
-                className=""
-              />
-            </a>
-            <div className="flex flex-col justify-start gap-2 p-2">
-              <h6 className="uppercase text-xs font-bold">Pre-order</h6>
-              <h4 className="text-sm">
-                NewJeans 2nd EP 'Get Up' Bunny Beach Bag ver. Random
-              </h4>
-              <h6 className="font-bold">$18.58</h6>
-            </div>
-          </li>
-          <li className=" max-w-[200px] flex flex-col gap-3">
-            <a href="">
-              <img
-                src="https://cdn-contents.weverseshop.io/public/shop/0a3506d263549998bc237e5f26e99597.png?q=95&w=720"
-                alt="/"
-                className=""
-              />
-            </a>
-            <div className="flex flex-col justify-start gap-2 p-2">
-              <h6 className="uppercase text-xs font-bold">Pre-order</h6>
-              <h4 className="text-sm">
-              NewJeans 2nd EP 'Get Up' THE POWERPUFF GIRLS X NJ Box ver. Random
-              </h4>
-              <h6 className="font-bold">$17.18</h6>
-            </div>
-          </li>
-          <li className="  max-w-[200px] flex flex-col gap-3">
-            <a href="">
-              <img
-                src="https://cdn-contents.weverseshop.io/public/shop/4d11e76df0007e851de705a70c936944.png?q=95&w=320"
-                alt="/"
-                className=""
-              />
-            </a>
-            <div className="flex flex-col justify-start gap-2 p-2">
-              <h6 className="uppercase text-xs font-bold">Pre-order</h6>
-              <h4 className="text-sm">
-              NewJeans 2nd EP 'Get Up' Weverse Albums ver. Set
-              </h4>
-              <h6 className="font-bold">$30.37</h6>
-            </div>
-          </li>
-          <li className="  max-w-[200px] flex flex-col gap-3">
-            <a href="">
-              <img
-                src="https://cdn-contents.weverseshop.io/public/shop/d986c27ae0b57d57abdc706a0bc81311.png?q=95&w=320"
-                alt="/"
-                className=""
-              />
-            </a>
-            <div className="flex flex-col justify-start gap-2 p-2">
-              <h6 className="uppercase text-xs font-bold">Pre-order</h6>
-              <h4 className="text-sm">
-              NewJeans 2nd EP 'Get Up' Weverse Albums ver. Random
-              </h4>
-              <h6 className="font-bold">$10.12</h6>
-            </div>
-          </li>
-          <li className="  max-w-[200px] flex flex-col gap-3">
-            <a href="">
-              <img
-                src="https://cdn-contents.weverseshop.io/public/shop/805beb69854a55c9d303b738c38c0b33.png?q=95&w=320"
-                alt="/"
-                className=""
-              />
-            </a>
-            <div className="flex flex-col justify-start gap-2 p-2">
-              <h6 className="uppercase text-xs font-bold">Pre-order</h6>
-              <h4 className="text-sm">
-              NewJeans 'OMG' Message Card ver. Set
-              </h4>
-              <h6 className="font-bold">$82.49</h6>
-            </div>
-          </li>
-          <li className="  max-w-[200px] flex flex-col gap-3">
-            <a href="">
-              <img
-                src="https://cdn-contents.weverseshop.io/public/shop/fec0d5a35b3d73bd1abbd0368e9d8268.png?q=95&w=320"
-                alt="/"
-                className=""
-              />
-            </a>
-            <div className="flex flex-col justify-start gap-2 p-2">
-              <h6 className="uppercase text-xs font-bold">Pre-order</h6>
-              <h4 className="text-sm">
-              NewJeans 'OMG' Message Card ver. Random
-              </h4>
-              <h6 className="font-bold">$13.74</h6>
-            </div>
-          </li>
-          <li className="  max-w-[200px] flex flex-col gap-3">
-            <a href="">
-              <img
-                src="https://cdn-contents.weverseshop.io/public/shop/13b8bc23633f83d9638d2ff0c4875d26.png?q=95&w=320"
-                alt="/"
-                className=""
-              />
-            </a>
-            <div className="flex flex-col justify-start gap-2 p-2">
-              <h6 className="uppercase text-xs font-bold">Pre-order</h6>
-              <h4 className="text-sm">
-              NewJeans 'OMG' Weverse Albums ver.
-              </h4>
-              <h6 className="font-bold">$10.12</h6>
-            </div>
-          </li>
-          <li className="  max-w-[200px] flex flex-col gap-3">
-            <a href="">
-              <img
-                src="https://cdn-contents.weverseshop.io/public/shop/b6096db086ffc1f815a0a07e3a1ad845.png?q=95&w=320"
-                alt="/"
-                className=""
-              />
-            </a>
-            <div className="flex flex-col justify-start gap-2 p-2">
-              <h6 className="uppercase text-xs font-bold">Pre-order</h6>
-              <h4 className="text-sm">
-              NewJeans 1st EP 'New Jeans' Bluebook ver. Set
-              </h4>
-              <h6 className="font-bold">$104.79</h6>
-            </div>
-          </li>
-          <li className="  max-w-[200px] flex flex-col gap-3">
-            <a href="">
-              <img
-                src="https://cdn-contents.weverseshop.io/public/shop/af01b408905514b485fe18e47b19b095.png?q=95&w=320"
-                alt="/"
-                className=""
-              />
-            </a>
-            <div className="flex flex-col justify-start gap-2 p-2">
-              <h6 className="uppercase text-xs font-bold">Pre-order</h6>
-              <h4 className="text-sm">
-              NewJeans 1st EP 'New Jeans' Bluebook ver. Random
-              </h4>
-              <h6 className="font-bold">$17.46</h6>
-            </div>
-          </li>
-          <li className="  max-w-[200px] flex flex-col gap-3">
-            <a href="">
-              <img
-                src="https://cdn-contents.weverseshop.io/public/shop/9912453e36490a90474afaf1f6e38606.png?q=95&w=320"
-                alt="/"
-                className=""
-              />
-            </a>
-            <div className="flex flex-col justify-start gap-2 p-2">
-              <h6 className="uppercase text-xs font-bold">Pre-order</h6>
-              <h4 className="text-sm">
-              NewJeans 1st EP 'New Jeans' Weverse Albums ver.
-              </h4>
-              <h6 className="font-bold">$10.12</h6>
-            </div>
-          </li>
-          <li className="  max-w-[200px] flex flex-col gap-3">
-            <a href="">
-              <img
-                src="https://cdn-contents.weverseshop.io/public/shop/6c7670b7efedcf95fb2fb3183df03aeb.png?q=95&w=320"
-                alt="/"
-                className=""
-              />
-            </a>
-            <div className="flex flex-col justify-start gap-2 p-2">
-              <h6 className="uppercase text-xs font-bold">Pre-order</h6>
-              <h4 className="text-sm">
-              NewJeans 1st EP 'New Jeans' Bag Red ver. LIMITED
-              </h4>
-              <h6 className="font-bold">$20.90</h6>
-            </div>
-          </li>
-          <li className="  max-w-[200px] flex flex-col gap-3">
-            <a href="">
-              <img
-                src="https://cdn-contents.weverseshop.io/public/shop/fffd2df89e9988cc4782167809ee1161.png?q=95&w=320"
-                alt="/"
-                className=""
-              />
-            </a>
-            <div className="flex flex-col justify-start gap-2 p-2">
-              <h6 className="uppercase text-xs font-bold">Pre-order</h6>
-              <h4 className="text-sm">
-              NewJeans 1st EP 'New Jeans' Bag Black ver. LIMITED
-              </h4>
-              <h6 className="font-bold">$20.90</h6>
-            </div>  
-          </li>
-          <li className="  max-w-[200px] flex flex-col gap-3">
-            <a href="">
-              <img
-                src="https://cdn-contents.weverseshop.io/public/shop/3d97f046d9e633e20def38ac7c98eb87.png?q=95&w=320"
-                alt="/"
-                className=""
-              />
-            </a>
-            <div className="flex flex-col justify-start gap-2 p-2">
-              <h6 className="uppercase text-xs font-bold">Pre-order</h6>
-              <h4 className="text-sm">
-              NewJeans 1st EP 'New Jeans' Bag (White) ver. (LIMITED)
-              </h4>
-              <h6 className="font-bold">$20.90</h6>
-            </div>
-          </li>
+        {Merch.map(({ imageUrl, merchName, price, status }) => {
+            return (
+              <li className="max-w-[200px] flex flex-col gap-3">
+                <a href="">
+                  <img src={imageUrl} alt="" />
+                </a>
+                <div className="flex flex-col justify-start gap-2 p-2">
+                  <h6 className="uppercase text-xs font-bold">{status}</h6>
+                  <h4 className="text-sm">
+                    {merchName}
+                  </h4>
+                  <h6 className="font-bold">{price}</h6>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
